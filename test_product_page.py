4d8e00f14eea6716beeb899756178c863a94f058
Petr Volkov <pvolkov20001130@gmail.com>
2021-09-21 17:21:38 +0300

    edit test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,18 +1,28 @@
 from pages.product_page import ProductPage
-import time
+import time, pytest
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
-    
+    #Запоминаем цену и название книги
     book_price = page.get_book_price()
     book_name = page.get_book_name()
-    
+    #Добавляем книгу в корзину
     page.should_be_add_product_to_basket()
     page.solve_quiz_and_get_code()
-    
+    #Проверяем имя и стоимость в сообщениях о добавлении в корзину
     page.should_be_book_name(book_name)
     page.should_be_book_price(book_price)
     
